0cdbd3069948b33f8cde079a18c692b3c81b201d
manos02 <64806766+manos02@users.noreply.github.com>
2025-12-23 23:42:30 +0100

    ex5_3 with map

--- MySolutions/3/tableformat.py
+++ MySolutions/3/tableformat.py
@@ -11,8 +11,18 @@ def convert_csv(lines, function):
         final_list.append(function(headers, row)) 
     return final_list                
        
-def make_dict(headers, row): return dict(zip(headers, row))
+def make_dict(headers, row):
+    return dict(zip(headers, row))
 
 
-print(convert_csv(lines, make_dict))
+# print(convert_csv(lines, make_dict))
+
+def convert_csv_map(lines, function):
+    lines = lines.read().split()  
+
+    lines = list(map(lambda x: x.split(","), lines))
+    headers = lines.pop(0)
+    return [function(headers, row) for row in lines]
+ 
+print(convert_csv_map(lines, make_dict))
 
